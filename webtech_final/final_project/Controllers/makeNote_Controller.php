3eda5a896db835265f1434ebf2363471331192cd
tahmeedalipatwary1 <80057732+tahmeedalipatwary1@users.noreply.github.com>
2024-05-14 11:04:27 +0600

    Delete webtech_final/final_project directory

--- webtech_final/final_project/Controllers/makeNote_Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['username'])) {
-    header("Location:../Controllers/logoutController.php");
-    exit();
-}
-header('Location: ../Views/make_note.php');
-?>
