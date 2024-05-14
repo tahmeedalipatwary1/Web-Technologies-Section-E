3c4358d9c2eed72353923752eac7496ebd5e99c9
tahmeedalipatwary1 <80057732+tahmeedalipatwary1@users.noreply.github.com>
2024-05-14 12:15:06 +0600

    Delete webtech_final/final_project directory

--- webtech_final/final_project/Controllers/logoutController.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-setcookie('last_activity', '', time() - 3600, '/');
-header("Location: ../Views/login_page.php");
-exit();
-?>
--- webtech_final/final_project/Views/JS/reset_activity.js
+++
@@ -1,9 +0,0 @@
-function resetActivityTimer() {
-    console.log("Activity timer reset.");
-    document.cookie = "last_activity=" + Math.floor(Date.now() / 1000) + ";max-age=3600;path=/";
-}
-
-window.onload = resetActivityTimer;
-document.onmousemove = resetActivityTimer;
-document.onkeypress = resetActivityTimer;
-document.onclick = resetActivityTimer;
